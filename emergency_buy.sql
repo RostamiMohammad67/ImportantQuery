84e3de9028d206b92c58b3eaedd715c069cbcf4d
RostamiMohammad67 <rostami_mohammad67@yahoo.com>
2025-01-01 11:23:37 +0330

    1403/10/12 اضافه شدن گزارش مربوط به خرید های اورژانسی به تفکیک قسمت

---
+++ emergency_buy.sql
@@ -0,0 +1,7 @@
+﻿--خرید های اورژانسی به تفکیک قسمت
+use Tadarokat
+
+select count(*) as count_emergency,rtrim(ltrim(Asker_ghesmat_name)) from Tadarokat_Buy
+where Emergency_acc=1 and Buy_request_datesabt>'1403/01/01'
+group by  Asker_ghesmat_code,Asker_ghesmat_name
+order by count_emergency desc
